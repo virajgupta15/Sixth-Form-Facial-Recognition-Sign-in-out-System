30fcfd5beaaf5a7020a0a813a6b0a14ad1ca4132
vman15 <115834377+vman15@users.noreply.github.com>
2023-09-20 11:48:56 +0100

    testing first commit

--- hog_face_detection.py
+++ hog_face_detection.py
@@ -1,8 +1,18 @@
 import dlib
 import cv2
 
+
 detector = dlib.get_frontal_face_detector()  # Load the pre-trained facial detection model from dlib
 cap = cv2.VideoCapture(0)  # Open the webcam (0 is the default camera)
+
+def find_largest_face(facesArray):
+    # finds the largest face detected in the frame and returns it index in the faceArray
+    largest_face_indx = max(range(len(facesArray)), key=lambda i:facesArray[i].area())
+    return largest_face_indx
+
+def draw_bounding_box(face):
+    x, y, w, h = face.left(), face.top(), face.width(), face.height()  # getting coordinates of the face
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
 while True:
     # Capture frame-by-frame
@@ -14,9 +24,9 @@ while True:
         faces = detector(gray)  # Detect faces in the grayscale frame using the dlib detector
 
         # Draw rectangles around the detected faces
-        for face in faces:
-            x, y, w, h = face.left(), face.top(), face.width(), face.height()  # getting coordinates of the face
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        if len(faces) > 0:
+            largest_face_indx = find_largest_face(faces)
+            draw_bounding_box(faces[largest_face_indx])
 
         # Display the resulting frame
         cv2.imshow('Facial Detection', frame)
